5d5f73bb216596dd301be937cd1722d3476b7281
Johan Groth <johan.groth@lime.tech>
2022-07-05 12:51:03 +0200

    WIP

--- src/mkdocstrings_handlers/typescript/handler.py
+++ src/mkdocstrings_handlers/typescript/handler.py
@@ -9,6 +9,10 @@ from markupsafe import Markup
 from mkdocs.exceptions import PluginError
 from mkdocstrings.handlers.base import BaseHandler, CollectionError, CollectorItem
 from mkdocstrings.loggers import get_logger
+from mkdocstrings_handlers.typescript import rendering
+import subprocess
+import os
+import json
 
 
 logger = get_logger(__name__)
@@ -47,17 +51,45 @@ class TypescriptHandler(BaseHandler):
     """  # noqa: E501
 
     def collect(self, identifier: str, config: dict) -> CollectorItem:  # noqa: D102
-        pass
+        if ':' not in identifier:
+            return {}
+
+        [package, name] = identifier.split(':', 1)
+
+        data = get_docs_data(package, name)
+
+        return Struct(data)
 
     def render(self, data: CollectorItem, config: dict) -> str:  # noqa: D102
-        pass
+        final_config = {**self.default_config, **config}
+        heading_level = final_config["heading_level"]
+        template = self.env.get_template(f"foo.html")
+        html = template.render(
+            **{
+                "config": final_config,
+                'class': data, 
+                'summary': get_summary(data), 
+                "heading_level": heading_level,
+                "root": True,
+                'methods': get_methods(data)
+            },
+        )
+        print('🎨', html)
+        return html
 
     def update_env(self, md: Markdown, config: dict) -> None:  # noqa: D102 (ignore missing docstring)
         super().update_env(md, config)
+        print('🌈', self.env.filters.keys())
         self.env.trim_blocks = True
         self.env.lstrip_blocks = True
         self.env.keep_trailing_newline = False
-        self.env.filters["crossref"] = self.do_crossref
+        # self.env.filters["crossref"] = self.do_crossref
+        self.env.filters["crossref"] = rendering.do_crossref
+        self.env.filters["multi_crossref"] = rendering.do_multi_crossref
+        self.env.filters["order_members"] = rendering.do_order_members
+        self.env.filters["format_code"] = rendering.do_format_code
+        self.env.filters["format_signature"] = rendering.do_format_signature
+        self.env.filters["filter_objects"] = rendering.do_filter_objects
 
     def do_crossref(self, path: str, brief: bool = True) -> Markup:
         """Filter to create cross-references.
@@ -100,3 +132,47 @@ def get_handler(
         custom_templates=custom_templates,
         config_file_path=config_file_path,
     )
+
+
+class Struct(object):
+    def __init__(self, data):
+        for name, value in data.items():
+            setattr(self, name, self._wrap(value))
+
+    def _wrap(self, value):
+        if isinstance(value, (tuple, list, set, frozenset)): 
+            return type(value)([self._wrap(v) for v in value])
+        else:
+            return Struct(value) if isinstance(value, dict) else value
+
+
+def get_docs_data(package, name):
+    json = gen_tsdoc(package)
+    
+    return next((item for item in json['children'] if item['name'] == name), None)
+
+
+def gen_tsdoc(package):
+    dir = os.path.dirname(os.path.abspath(__file__)) + '/tsdoc'
+    cwd = '/home/jth/workspace/lime-web-components/packages/lime-web-components'
+    process = subprocess.Popen([
+        'npx',
+        'typedoc',
+        '--options',
+        f'{dir}/typedoc.json',
+        '--entryPoints',
+        'src/index.ts'
+    ], cwd=cwd)
+    process.wait()
+
+    with open(f'{dir}/docs.json') as fp:
+        return json.load(fp)
+
+
+def get_summary(data):
+    text = ''.join([summary.text for summary in data.comment.summary])
+
+    return text
+
+def get_methods(data):
+    return [m for m in data.children if m.kind == 2048]
